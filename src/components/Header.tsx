6ee328b21b73c2cbade5b15fdef5d1b973014aa7
professionalchandru <professionalchandru@gmail.com>
2025-08-31 20:41:05 +0530

    Initial Auth added. Installed Kinde for email auth. Setuped a Kinde in project. Created middleware and added the protected page. Added logout page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,52 @@
 import React from 'react';
 import NavButton from '@/components/NavButton';
-import { File, HomeIcon, UsersRound } from 'lucide-react';
+import { File, HomeIcon, LogOut, UsersRound } from 'lucide-react';
 import Link from 'next/link';
 import { ModeToggle } from '@/components/ModeToggle';
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components';
+import { Button } from '@/components/ui/button';
 
 export default function Header() {
-  return (
-    <header className="animate-slide bg-background h-12 p-2 border-b sticky top-0 z-20">
-      <div className="flex h-8 items-center justify-between w-full">
-        <div className="flex items-center gap-2">
-          <NavButton icon={HomeIcon} label="Home" href="/home" />
-          <Link
-            className="flex justify-center items-center gap-2 ml-0"
-            title="Home"
-            href={'/home'}
-          >
-            <h1 className="hidden sm:block text-xl font-bold m-0 mt-1">
-              Computer Repair Shop
-            </h1>
-          </Link>
-        </div>
+    return (
+        <header className="animate-slide bg-background h-12 p-2 border-b sticky top-0 z-20">
+            <div className="flex h-8 items-center justify-between w-full">
+                <div className="flex items-center gap-2">
+                    <NavButton icon={HomeIcon} label="Home" href="/home" />
+                    <Link
+                        className="flex justify-center items-center gap-2 ml-0"
+                        title="Home"
+                        href={'/home'}
+                    >
+                        <h1 className="hidden sm:block text-xl font-bold m-0 mt-1">
+                            Computer Repair Shop
+                        </h1>
+                    </Link>
+                </div>
 
-        <div className="flex items-center">
-          <NavButton icon={File} label="Tickets" href="/tickets" />
+                <div className="flex items-center">
+                    <NavButton icon={File} label="Tickets" href="/tickets" />
 
-          <NavButton icon={UsersRound} label="Customers" href="/customers" />
+                    <NavButton
+                        icon={UsersRound}
+                        label="Customers"
+                        href="/customers"
+                    />
 
-          <ModeToggle />
-        </div>
-      </div>
-    </header>
-  );
+                    <ModeToggle />
+                    <Button
+                        variant={'ghost'}
+                        aria-label="Logout"
+                        title="Logout"
+                        className="rounded-full"
+                        size={'icon'}
+                        asChild
+                    >
+                        <LogoutLink>
+                            <LogOut />
+                        </LogoutLink>
+                    </Button>
+                </div>
+            </div>
+        </header>
+    );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+export default withAuth(
+    async function middleware(request: NextRequest) {
+        //console.log("requests log", request)
+    }, {
+    isReturnToCurrentPage: true
+}
+)
+
+export const config = {
+    matcher: [
+        /**
+         * Match all the requset paths except for ones starting with:
+         * - api (Api routes)
+         * - _next/static (static files)
+         * - _next/images (image optimization files)
+         * - Auth
+         * - favicon
+         * - robots.txt
+         * - images
+         * - login
+         * - homepage (represented with $ after beginning)
+         * 
+         */
+        '/((?!api|_next/static|_next/image|auth|favicon.ico|robots.txt|images|login|$).*)'
+    ]
+}
