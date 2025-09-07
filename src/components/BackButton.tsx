5b77c3a9bb3ed8f7c44ed8f2e290275250982981
professionalchandru <professionalchandru@gmail.com>
2025-09-07 20:44:31 +0530

    created get queries for customer and tickets. created form routes for tickets and customer. added the negative case if the customer or ticket id not found. back button component created.

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+import { ButtonHTMLAttributes } from 'react';
+
+type Props = {
+    title: string;
+    className?: string;
+    variant?:
+        | 'default'
+        | 'destructive'
+        | 'outline'
+        | 'secondary'
+        | 'ghost'
+        | 'link'
+        | null
+        | undefined;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function BackButton({ title, className, variant, ...props }: Props) {
+    const router = useRouter();
+    return (
+        <Button
+            variant={variant}
+            className={className}
+            onClick={() => router.back()}
+            title={title}
+            {...props}
+        >
+            {title}
+        </Button>
+    );
+}
